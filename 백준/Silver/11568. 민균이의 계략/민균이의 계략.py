bfa555dbe466e5c36f92c4393fddc8be82dd984e
김명기 <riroan@naver.com>
2022-05-29 14:37:24 +0900

    [Silver II] Title: 민균이의 계략, Time: 132 ms, Memory: 114408 KB -BaekjoonHub

---
+++ 백준/Silver/11568. 민균이의 계략/민균이의 계략.py
@@ -0,0 +1,14 @@
+import bisect
+
+def lis(n, arr):
+    brr = [-9876543210]
+    for i in range(n):
+        if arr[i] > brr[-1]:
+            brr.append(arr[i])
+            continue
+        t = bisect.bisect_left(brr, arr[i])
+        brr[t] = arr[i]
+    return brr
+n=int(input())
+arr = list(map(int, input().split()))
+print(len(lis(n, arr))-1)
