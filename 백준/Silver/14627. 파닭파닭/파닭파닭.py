ace7e8a00ad8ddb12afaf7abbc8f33f04670ea54
김명기 <riroan@naver.com>
2022-05-30 11:39:05 +0900

    [Silver III] Title: 파닭파닭, Time: 1108 ms, Memory: 124240 KB -BaekjoonHub

---
+++ 백준/Silver/14627. 파닭파닭/파닭파닭.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+arr = [int(input()) for i in range(n)]
+
+
+def check(x):
+    s = 0
+    for i in arr:
+        s += i//x
+    return s
+
+
+l = 0
+r = 2**32
+while l + 1 < r:
+    mid = (l+r)//2
+    k = check(mid)
+    if k >= m:
+        l = mid
+    else:
+        r = mid
+while check(mid) < m:
+    mid -= 1
+ans = 0
+ans += sum(arr) - mid * m
+print(ans)
