400323a1f99360233994ffce023636072cf1b70e
김명기 <riroan@naver.com>
2022-07-24 23:53:57 +0900

    [Gold IV] Title: 퍼레이드, Time: 0 ms, Memory: 2216 KB -BaekjoonHub

---
+++ 백준/Gold/16168. 퍼레이드/퍼레이드.cc
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#define endl '\n'
+const int oOvOo = 0;
+using ll = long long;
+using namespace std;
+class DisjointSet{
+public:
+    vector<int> p;
+    int n;
+    DisjointSet(int _n) : n(_n){
+        p.resize(n);
+        iota(p.begin(), p.end(), 0);
+    }
+
+    inline int get(int x)
+    {
+        return (x == p[x] ? x : (p[x] = get(p[x])));
+    }
+
+    inline bool unite(int x, int y)
+    {
+        x = get(x);
+        y = get(y);
+        if (x != y)
+        {
+            p[x] = y;
+            return true;
+        }
+        return false;
+    }
+};
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> g(n);
+    DisjointSet ds(n);
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        g[a].push_back(b);
+        g[b].push_back(a);
+        ds.unite(a, b);
+    }
+    bool ok = true;
+    for (int i = 0; i < n;i++){
+        if (ds.get(i) != ds.get(0))
+        ok = false;
+    }
+        int ocnt = 0, ecnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if(g[i].size()%2 == 0)
+            ecnt++;
+        else
+            ocnt++;
+    }
+    if((ocnt == 0 || ocnt == 2)&&ok){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+    return oOvOo;
+}
