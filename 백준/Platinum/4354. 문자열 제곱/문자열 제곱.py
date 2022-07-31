1b211eefdd191d8b09f2082abf44edbe3694fb4a
김명기 <riroan@naver.com>
2022-07-31 21:53:07 +0900

    [Platinum V] Title: 문자열 제곱, Time: 920 ms, Memory: 235872 KB -BaekjoonHub

---
+++ 백준/Platinum/4354. 문자열 제곱/문자열 제곱.py
@@ -0,0 +1,34 @@
+import sys
+def input(): return sys.stdin.readline().strip()
+
+
+def kmp(a, b):
+    lb = len(b)
+    fail = [0]*lb
+    j = 0
+    for i in range(1, lb):
+        while j > 0 and b[i] != b[j]:
+            j = fail[j-1]
+        if b[i] == b[j]:
+            j += 1
+            fail[i] = j
+    ans = []
+    j = 0
+    for i in range(len(a)):
+        while j and a[i] != b[j]:
+            j = fail[j-1]
+        if a[i] == b[j]:
+            if j == lb-1:
+                ans.append(i-lb+2)
+                j = fail[j]
+            else:
+                j += 1
+    return ans
+
+
+while True:
+    s = input()
+    if s == ".":
+        break
+    a,b = kmp(s+s,s)[:2]
+    print(len(s)//(b-a))
