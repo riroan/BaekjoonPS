1653a29ea93ca772cdcce8fd17a3eebdf9e0ffbc
김명기 <riroan@naver.com>
2024-03-26 20:22:47 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 0 ms, Memory: 2020 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.cc
@@ -0,0 +1,20 @@
+/**
+ *    author:  riroan
+ *    created:  2024.03.26 20:22:12
+ **/
+#include <bits/stdc++.h>
+#define endl '\n'
+#define int long long
+using namespace std;
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a * b + c;
+
+    return 0;
+}
